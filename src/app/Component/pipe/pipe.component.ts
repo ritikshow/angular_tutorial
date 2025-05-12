5380611cf5ba424092c232bd47b5524cacae9d10
ritikshow <ritikbaryons@gmail.com>
2025-05-12 07:30:14 +0530

    popup

--- src/app/Component/pipe/pipe.component.ts
+++ src/app/Component/pipe/pipe.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-pipe',
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./pipe.component.css']
 })
 export class PipeComponent {
+  showNotification = false;
 
+  notifications = [
+    { tableNo: 'SF08', orderNo: 246, message: 'Items Are Prepared!' },
+    { tableNo: 'SF04', orderNo: 245, message: 'Items Are Prepared!' },
+    { tableNo: 'SF03', orderNo: 244, message: 'Items Are Prepared!' },
+    { tableNo: 'Online', orderNo: 243, message: 'Items Are Prepared!' },
+    { tableNo: 'Online', orderNo: 240, message: 'Items Are Prepared!' },
+    { tableNo: 'SF04', orderNo: 242, message: 'Items Are Prepared!' }
+  ];
+
+  constructor(private _eref: ElementRef) {}
+
+  toggleNotification() {
+    this.showNotification = !this.showNotification;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    if (!this._eref.nativeElement.contains(event.target)) {
+      this.showNotification = false;
+    }
+  }
 }
